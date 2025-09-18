c27a313f677d8fe9fdf8fed625ffc786888b9320
LateOne15 <48002046+LateOne15@users.noreply.github.com>
2025-09-18 15:26:28 +0800

    1.5 GUI, Sequential, StatisticData
    Complete GUI and Statistic data implementation, plus data refresh and Sequential search functionality. Currently existing bug for Mode display when multi-mode list.

--- AstronomicalProcessing/Astronomical/MainWindow.xaml.cs
+++ AstronomicalProcessing/Astronomical/MainWindow.xaml.cs
@@ -1,14 +1,18 @@
 ﻿using System.CodeDom;
 using System.ComponentModel;
+using System.Data;
 using System.Diagnostics.Metrics;
+using System.Globalization;
 using System.Text;
 using System.Windows;
+using System.Windows.Automation.Provider;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -29,6 +33,7 @@ namespace Astronomical
         public MainWindow()
         {
             InitializeComponent();
+            tbtnBinary.IsChecked = true;
             int[] ints = new int[24];
             Random rnd = new Random();
             for (int i = 0; i < 24; i++) // randomly assigns 24 values to an array with min 10 and max 90
@@ -58,6 +63,7 @@ namespace Astronomical
         //}
         #endregion
 
+        #region SortSearch
         private List<int> SortList(List<int> ints) // Bubble Sort
         {
             int iterations = ints.Count; 
@@ -138,20 +144,106 @@ namespace Astronomical
             //tbSearch.Text = string.Empty;
         }
 
+        private int SequentialSearch (int x)
+        {
+            List<int> ints = new List<int>();
+            foreach (int i in lbxDataList.Items) //moves the datalist items into new array
+            {
+                ints.Add(i);
+            }
+            for (int i = 0; i < ints.Count; i++) // iterates through every member of the unsorted array
+            {
+                if (ints[i] == x)
+                {
+                    return i; // if a match is found, return the index
+                }
+            }
+            return -1;
+        }
+
+        #endregion
+
+        #region Math
+        private double AverageMean(List<int> ints) // mean calculation
+        {
+            double sum = 0;
+            foreach (int i in ints) // sum all elements
+            {
+                sum += i;
+            }
+            double mean = sum / ints.Count; // divide by count
+            return mean;
+        }
+
+        private List<int> AverageMode(List<int> ints)
+        {
+            List<int> mode = new List<int>();
+            mode.Add(1);
+            int duplicates = 1, mostFrequent = 0;
+            int[] frequency = new int[ints.Last() + 1];
+            for (int i=0;i<frequency.Length;i++)
+            {
+                frequency[i] = 0;
+            }
+            foreach (int i in ints)
+            {
+                frequency[i]++;
+            }
+            for (int i=0;i<frequency.Length;i++)
+            {
+                if (frequency[i] > mostFrequent)
+                {
+                    mostFrequent = frequency[i];
+                    mode.Clear();
+                    mode.Add(i);
+                }
+                else if (frequency[i] == mostFrequent)
+                {
+                    mode.Add(i);
+                }
+            }
+            return mode;
+        }
+
+        private double AverageRange (List<int> ints)
+        {
+            double range = ints.Last() - ints.First();
+            return range;
+        }
+
+        private double AverageMidExtreme (List<int> ints)
+        {
+            double midExtreme = (ints.Last() + ints.First()) / 2;
+            return midExtreme;
+        }
+
+        #endregion
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
             string searchTerm = tbSearch.Text;
             if (int.TryParse(searchTerm, out int searchInt)) // checks if the input is valid first
             {
-                int success = BinarySearch(searchInt); // method for binary searching
+                int success = -2;
+                if (tbtnBinary.IsChecked == true)
+                {
+                    success = BinarySearch(searchInt); // method for binary searching
+                }
+                else if (tbtnSequential.IsChecked == true)
+                {
+                    success = SequentialSearch(searchInt); // method for sequential searching
+                }
                 if (success != -1) // -1 is an error return, means no match found
                 {
                     lbxDataList.SelectedItem = lbxDataList.Items.GetItemAt(success);
                     DisplayMessage($"Match found at index {lbxDataList.SelectedIndex}!", "Found"); // tells index number starting at 0
                 }
-                else // in case of no match
+                else if (success == -1) // in case of no match
                 {
                     DisplayError("No match found in list.", "Unsuccessful");
+                }
+                else // in case of somehow entering method with neither search method checked
+                {
+                    DisplayError("No search method selected.", "Error");
                 }
             }
             else // input error display
@@ -206,6 +298,79 @@ namespace Astronomical
                 DisplayError("Please enter a valid integer.","Input Error");
             }
             tbEdit.Text = string.Empty; // removes the edit text
+        }
+
+        private void tbtn_Checked(object sender, RoutedEventArgs e)
+        {
+            if (sender == tbtnBinary)
+            {
+                tbtnSequential.IsChecked = false;
+            }
+            else if (sender == tbtnSequential)
+            {
+                tbtnBinary.IsChecked = false;
+            }
+        }
+
+        private void tbtn_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (sender == tbtnBinary)
+            {
+                tbtnSequential.IsChecked = true;
+            }
+            else if (sender == tbtnSequential)
+            {
+                tbtnBinary.IsChecked = true;
+            }
+        }
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            lbxDataList.Items.Clear();
+            int[] ints = new int[24];
+            Random rnd = new Random();
+            for (int i = 0; i < 24; i++) // randomly assigns 24 values to an array with min 10 and max 90
+            {
+                ints[i] = rnd.Next(10, 91);
+            }
+            foreach (int i in ints) //sets the list to those 24 items
+            {
+                lbxDataList.Items.Add(i);
+            }
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            List<int> ints = new List<int>();
+            foreach (int i in lbxDataList.Items) //moves the datalist items into new array
+            {
+                ints.Add(i);
+            }
+            ints = SortList(ints);
+            double mean = AverageMean(ints);
+            List<int> mode = AverageMode(ints);
+            double range = AverageRange(ints);
+            double midExtreme = AverageMidExtreme(ints);
+
+            tbMean.Text = mean.ToString("F", CultureInfo.InvariantCulture);
+            if (mode.Count > 1)
+            {
+                string modeText = string.Empty;
+                foreach (int i in mode)
+                {
+                    modeText += i.ToString("F",CultureInfo.InvariantCulture);
+                    if (i != mode.Last())
+                    {
+                        modeText += ", ";
+                    }
+                }
+                tbMode.Text = modeText;
+            }
+            else
+            {
+                tbMode.Text = mode.First().ToString("F", CultureInfo.InvariantCulture);
+            }
+            tbRange.Text = range.ToString("F", CultureInfo.InvariantCulture);
+            tbMidExtreme.Text = midExtreme.ToString("F", CultureInfo.InvariantCulture);
         }
     }
 }
